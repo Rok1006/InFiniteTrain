bc07886e2c251ac3e1074cfefe78c3f7f58595b8
ReynahD <wl2357@nyu.edu>
2022-09-26 12:10:57 -0400

    asdf
    asdf

--- TrainGame/Assets/CharacterRotationPlus.cs
+++ TrainGame/Assets/CharacterRotationPlus.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CharacterRotationPlus : MonoBehaviour
 {
+    public float spinspeed;
+    public bool isMoving = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,14 @@ public class CharacterRotationPlus : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A) && isMoving == false)
+        {
+            //isMoving = true;
+            gameObject.transform.eulerAngles = new Vector3(0, -180, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D)){
+            gameObject.transform.eulerAngles = new Vector3(0, 0, 0);
+        }
     }
 }
