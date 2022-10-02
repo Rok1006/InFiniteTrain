e72b584a7806e260a2df611beab16f78c6ce9359
ReynahD <wl2357@nyu.edu>
2022-10-02 18:50:14 -0400

    map stuff update

--- TrainGame/Assets/Scripts/NodeManager.cs
+++ TrainGame/Assets/Scripts/NodeManager.cs
@@ -9,12 +9,26 @@ public class NodeManager : MonoBehaviour
     public NodeTypes[] nodeType;
     public LayerMask layermask;
     public List<GameObject> nearbyNodesList = new List<GameObject>();
+    public Material material;
+    public bool isPlayer;
+    public NodeTypes player;
     // Start is called before the first frame update
     void Start()
     {
-        node = new Node(nodeType[Random.Range(0,nodeType.Length )]);
-        sprite = node.sprite;
-        this.gameObject.GetComponent<SpriteRenderer>().sprite = sprite;
+        if (isPlayer == false)
+        {
+
+
+            node = new Node(nodeType[Random.Range(0, nodeType.Length)]);
+            sprite = node.sprite;
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = sprite;
+        }
+        else
+        {
+            node = new Node(player);
+            sprite = node.sprite;
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = sprite;
+        }
     }
 
     // Update is called once per frame
@@ -31,5 +45,17 @@ public class NodeManager : MonoBehaviour
             Debug.Log("hit");
             nearbyNodesList.Add(hit.collider.gameObject);
         }
+    }
+    public void DrawLinesNodes()
+    {
+        GameObject myLine = new GameObject();
+        myLine.transform.position = this.transform.position;
+        myLine.AddComponent<LineRenderer>();
+        LineRenderer lr = myLine.GetComponent<LineRenderer>();
+        lr.material = material;
+        lr.SetColors(Color.white, Color.white);
+        lr.SetWidth(0.042f, 0.042f);
+        lr.SetPosition(0, this.transform.position);
+        lr.SetPosition(1, nearbyNodesList[0].transform.position);
     }
 }
--- TrainGame/Assets/Scripts/SpawnNodes.cs
+++ TrainGame/Assets/Scripts/SpawnNodes.cs
@@ -8,22 +8,33 @@ public class SpawnNodes : MonoBehaviour
     public List<GameObject> nodes = new List<GameObject>();
     public GameObject nodePrefab;
     private Vector3 offset;
+    public GameObject[] spawnPoints;
+    public GameObject player;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < 20; i++)
+        Instantiate(player, transform.position, transform.rotation);
+        for (int j = 0; j < spawnPoints.Length; j++)
         {
-            var randomX = Random.Range(-10, 10);
-            var randomY = Random.Range(-10, 10);
-            offset = new Vector3(randomX, randomY, 0);
-            var node =Instantiate(nodePrefab, transform.position + offset, transform.rotation);
-            nodes.Add(node);
-           
 
+
+            for (int i = 0; i < 4; i++)
+            {
+                var randomX = Random.Range(-4, 4);
+                var randomY = Random.Range(-4, 4);
+                offset = new Vector3(randomX, randomY, 0);
+                var node = Instantiate(nodePrefab, spawnPoints[i].transform.position + offset, transform.rotation);
+                nodes.Add(node);
+
+
+            }
         }
         foreach(GameObject go in nodes)
         {
             go.GetComponent<NodeManager>().ConnectNodes();
+            go.GetComponent<NodeManager>().DrawLinesNodes();
         }
 
         
