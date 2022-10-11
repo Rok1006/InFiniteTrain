4509801c1aecbef0278d1751ad1c61e15640a0b5
ROK <hilary0310@gmail.com>
2022-10-11 12:53:01 -0400

    Animation Fix

--- TrainGame/Assets/Scripts/PlayerMC.cs
+++ TrainGame/Assets/Scripts/PlayerMC.cs
@@ -41,14 +41,24 @@ public class PlayerMC : MonoBehaviour
     void FixedUpdate() {
         moveInput.x = Input.GetAxisRaw("Horizontal");
         moveInput.y = Input.GetAxisRaw("Vertical");
-        moveInput.Normalize();
+       // moveInput = Vector2.zero;
+        // if(Input.GetKeyDown(KeyCode.A)){
+        //     moveInput += Vector2.left;
+        // }else if(Input.GetKeyDown(KeyCode.D)){
+        //     moveInput += Vector2.right;
+        // }else if(Input.GetKeyDown(KeyCode.W)){
+        //     moveInput += Vector2.up;
+        // }else if(Input.GetKeyDown(KeyCode.S)){
+        //     moveInput += Vector2.down;
+        // }else{moveInput = Vector2.zero;}
 
+        moveInput.Normalize();
         rb.velocity = new Vector3(-moveInput.x*moveSpeed, rb.velocity.y, -moveInput.y*moveSpeed); //rb.velocity.y
     }
 
     void Update()
     {
-        
+
         if (transform.position.x > oldPositionX) //Rotate the anim object instead of main
         {
             FrontMC.transform.eulerAngles = new Vector3(0, 0, 0);
@@ -57,29 +67,32 @@ public class PlayerMC : MonoBehaviour
         {
             FrontMC.transform.eulerAngles = new Vector3(0, 180, 0);
         }
-
-        if (moveInput.x == 0&&moveInput.y == 0)   //for animation just check the teacher sample
+        
+//Mpvenment--------
+        if(transform.position.z > oldPositionZ || transform.position.z < oldPositionZ) //up and down
         {
+            facingFront = true;
+            DisableAllAnim();
+            frontAnim.SetBool("front_walk", true);
+            frontAnim.SetBool("front_idle", false);  
+            Debug.Log(oldPositionX + "-"+ transform.position.x);
+        }else if(transform.position.x > oldPositionX || transform.position.x < oldPositionX){ //left and right
+            facingFront = false;
+            DisableAllAnim();
+            frontAnim.SetBool("side_walk", true);
+            frontAnim.SetBool("side_idle", false); 
+        }  
+        if(moveInput.x == 0 && moveInput.y == 0){ //player stop
             if(facingFront){
                 DisableAllAnim();
                 frontAnim.SetBool("front_idle", true);  
-                //frontAnim.SetBool("front_idle", true);  
             }else{
                 DisableAllAnim();
                 frontAnim.SetBool("side_idle", true);  
             }
-        }
-        else if(Input.GetKeyDown(KeyCode.DownArrow)&& transform.position.z > oldPositionZ || Input.GetKeyDown(KeyCode.UpArrow)&& transform.position.z < oldPositionZ)
-        {
-            DisableAllAnim();
-            frontAnim.SetBool("front_walk", true);
-            frontAnim.SetBool("front_idle", false);  
-            facingFront = true;
-        }else if(Input.GetKeyDown(KeyCode.LeftArrow)&& transform.position.x > oldPositionX || Input.GetKeyDown(KeyCode.RightArrow)&& transform.position.x < oldPositionX){
-            DisableAllAnim();
-            frontAnim.SetBool("side_walk", true);
-            frontAnim.SetBool("side_idle", false); 
-            facingFront = false;
+        }else{
+            frontAnim.SetBool("side_idle", false);  
+            frontAnim.SetBool("front_idle", false); 
         }
         oldPositionX = transform.position.x;
         oldPositionZ = transform.position.z;
