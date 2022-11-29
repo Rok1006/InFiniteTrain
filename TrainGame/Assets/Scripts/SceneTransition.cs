3a2a975dfa98f44898be4e94604db3e667171fbf
ReynahD <wl2357@nyu.edu>
2022-11-29 17:24:54 -0500

    added door room

---
+++ TrainGame/Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using MoreMountains.Tools;
+using MoreMountains.Feedbacks;
+
+public class SceneTransition : MonoBehaviour
+{
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            SceneManager.LoadScene("Map Test");
+        }
+        
+    }
+    public void ToBattleScene()
+    {
+        SceneManager.LoadScene("Map Test");
+    }
+}
