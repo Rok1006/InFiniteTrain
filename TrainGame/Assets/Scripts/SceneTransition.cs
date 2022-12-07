1f3106658ce11973123816b13a09355e83f5e0e9
Leo Ji <sj3026@nyu.edu>
2022-12-07 01:26:27 -0500

    player now can go to battle scene through the door at the engine room

--- TrainGame/Assets/Scripts/SceneTransition.cs
+++ TrainGame/Assets/Scripts/SceneTransition.cs
@@ -7,17 +7,13 @@ using MoreMountains.Feedbacks;
 
 public class SceneTransition : MonoBehaviour
 {
-
-    // public void OnTriggerEnter(Collider other)
-    // {
-    //     if(other.tag == "Player")
-    //     {
-    //         SceneManager.LoadScene("Map Test");
-    //     }
-        
-    // }
     public void ToBattleScene()
     {
         SceneManager.LoadScene("Map Test");
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.GetComponent<PlayerManager>() != null)
+            ToBattleScene();
     }
 }
