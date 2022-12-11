15e24cc548302b2700ae9f17634d8fd1356f3a58
Leo Ji <sj3026@nyu.edu>
2022-12-11 01:54:32 -0500

    dont know why there's so much change but just pushed it

--- TrainGame/Assets/Scripts/AIActionTeleport.cs
+++ TrainGame/Assets/Scripts/AIActionTeleport.cs
@@ -17,7 +17,11 @@ public class AIActionTeleport : AIAction
     }
     private void Teleport()
     {
-        Debug.Log("tp");
+        // Debug.Log("tp");
+        if (_brain == null) Debug.Log("_brain is null");
+        if (_brain.Target == null) Debug.Log("_brain.Target is null");
+        if (_brain.Target.GetComponent<PlayerInformation>() == null) Debug.Log("_brain.information is null");
+        if (_brain.Target.GetComponent<PlayerInformation>().back == null) Debug.Log("_brain.back is null");
         var tpPoint = _brain.Target.GetComponent<PlayerInformation>().back;
         var target = _brain.Target;
         gameObject.transform.position = tpPoint.position;
