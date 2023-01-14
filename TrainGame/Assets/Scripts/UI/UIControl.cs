af841b7a9afdb4b828999575225afe2c6c8da49c
Leo Ji <sj3026@nyu.edu>
2023-01-14 11:30:32 -0500

    ui control script add train map set up function

--- TrainGame/Assets/Scripts/UI/UIControl.cs
+++ TrainGame/Assets/Scripts/UI/UIControl.cs
@@ -9,5 +9,33 @@ using NaughtyAttributes;
 public class UIControl : MonoBehaviour
 {
     [SerializeField, BoxGroup("World Map")] private GameObject worldMap;
+<<<<<<< Updated upstream
     [SerializeField, BoxGroup("Train Map")] private GameObject trainIconPanel, playerIcon;
+=======
+    [SerializeField, BoxGroup("Train Map")] private GameObject trainMap, trainIconPanel, playerIcon;
+    [SerializeField, BoxGroup("Train Map"), InfoBox("order from far right to far left", EInfoBoxType.Normal)] private List<TrainMapTrainIcon> trainMapTrainIcons;
+
+    //managers references
+    private RoomManager roomManager;
+
+    void Start() {
+        roomManager = FindObjectOfType<RoomManager>();
+        trainMapSetUp();
+    }
+
+    private void trainMapSetUp() {
+        for (int i = 0; i < roomManager.CarList.Count; i++) {
+            if (trainMapTrainIcons.Count < i)
+                roomManager.CarList[i].TrainMapIcon = trainMapTrainIcons[i];
+            else
+                Debug.Log("there's more car (" + roomManager.CarList.Count + ") than train map train icons (" + trainMapTrainIcons.Count +") ");
+        }
+    }
+
+    /*  inside train map
+        move player icon to corresponding car's train map train icon*/
+    public void moveTrainMapPlayerIcon(Car car) {
+
+    }
+>>>>>>> Stashed changes
 }
