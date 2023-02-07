5651d9a306cbc2673ffd6a4a4acc1b690c24aa5a
ROK <hilary0310@gmail.com>
2023-02-07 02:21:51 -0500

    map

--- TrainGame/Assets/Scripts/MapState.cs
+++ TrainGame/Assets/Scripts/MapState.cs
@@ -8,7 +8,9 @@ public class MapState : MonoBehaviour
 {
 
     public static int gameState;
-    LineRenderer LR;
+    [SerializeField] private LineRenderer OW_LR;
+    [SerializeField] private LineRenderer TW_LR;
+    //LineRenderer LR;
     [SerializeField] private List<GameObject> Point = new List<GameObject>();
     // public GameObject UI;
     // public BoxCollider doorTrigger;
@@ -16,16 +18,24 @@ public class MapState : MonoBehaviour
     // public GameObject text1;
     void Start()
     {
-        LR = this.GetComponent<LineRenderer>();
-        LR.positionCount = 2;
+        //TW_LR = this.GetComponent<LineRenderer>();
+        TW_LR.positionCount = Point.Count;
+        for (int i = 0; i<= Point.Count; i++){
+            TW_LR.SetPosition(i, new Vector3(Point[i].transform.position.x, Point[i].transform.position.y, Point[i].transform.position.z));
+        }
         // doorTrigger.enabled = false;
         // block.enabled = true;
         // text1.SetActive(false);
     }
     void Update()
     {
-        LR.SetPosition(0, new Vector3(Point[0].transform.position.x, Point[0].transform.position.y, Point[0].transform.position.z));
-        LR.SetPosition(1, new Vector3(Point[1].transform.position.x, Point[1].transform.position.y, Point[1].transform.position.z));
+        // for (int i = 0; i<= Point.Count; i++){
+        //     TW_LR.SetPosition(i, new Vector3(Point[i].transform.position.x, Point[i].transform.position.y, Point[i].transform.position.z));
+        // }
+
+        // TW_LR.SetPosition(0, new Vector3(Point[0].transform.position.x, Point[0].transform.position.y, Point[0].transform.position.z));
+        // TW_LR.SetPosition(1, new Vector3(Point[1].transform.position.x, Point[1].transform.position.y, Point[1].transform.position.z));
+        // TW_LR.SetPosition(2, new Vector3(Point[2].transform.position.x, Point[2].transform.position.y, Point[2].transform.position.z));
     //    if(gameState >= 1)
     //     {
     //         UI.GetComponent<TextMeshProUGUI>().text = "Door Unlocked";
