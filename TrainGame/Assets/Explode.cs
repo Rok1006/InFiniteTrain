ae89de67181b946788c65e0f7f4ac1ffc2300fed
ReynahD <wl2357@nyu.edu>
2023-02-22 21:21:14 -0500

    exploding drone

--- TrainGame/Assets/Explode.cs
+++ TrainGame/Assets/Explode.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Explode : MonoBehaviour
 {
+
+    public GameObject player;
+    public RadiationManager rm;
+    public GameObject particles;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
+        rm = GameObject.Find("GameManager").GetComponent<RadiationManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(Vector3.Distance(this.transform.position, player.transform.position));
+        if(Vector3.Distance(this.transform.position , player.transform.position )< 0.5f){
+            rm.CurrentRadiationLevel++;
+            Instantiate(particles, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        }
     }
 }
