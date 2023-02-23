b3ba4c871a1cacba5e70d9920135ff79758c9c91
ReynahD <wl2357@nyu.edu>
2023-02-22 22:28:18 -0500

    crispy laser dude

--- TrainGame/Assets/LaserDrone.cs
+++ TrainGame/Assets/LaserDrone.cs
@@ -14,17 +14,20 @@ public class LaserDrone : MonoBehaviour
         
     }
     public LayerMask layermask;
-    private int destPoint = 0;
+    public int destPoint = 0;
     public float speed;
     private bool stop;
     private float chance = 0.4f;
     public State state;
     public GameObject player;
     public float range;
+    public float fovAngle;
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        rb = this.gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -48,15 +51,27 @@ public class LaserDrone : MonoBehaviour
 
     void Move()
     {
+
+        if(rb.velocity.x > 0)
+        {
+            this.gameObject.transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        else
+        {
+            this.gameObject.transform.eulerAngles = new Vector3(0, 180, 0);
+        }
         if(wayPoints.Length == 0)
         {
             return;
         }
-        transform.position = Vector3.MoveTowards(transform.position, wayPoints[destPoint].position, speed);
+        //transform.position = Vector3.MoveTowards(transform.position, wayPoints[destPoint].position, speed);
+        var direction = (wayPoints[destPoint].position - this.transform.position).normalized;
+        
+        rb.velocity = direction * speed;
 
         var currentDestination = wayPoints[destPoint].position;
 
-        if(Vector3.Distance(transform.position , currentDestination) < 0.5f)
+        if(Vector3.Distance(transform.position , currentDestination) < 0.8f)
         {
             destPoint = (destPoint + 1) % wayPoints.Length;
             var stopChance = Random.Range(0, 1f);
@@ -76,7 +91,7 @@ public class LaserDrone : MonoBehaviour
         float totalTime = 0;
         while (totalTime <= duration)
         {
-            
+            rb.velocity = Vector3.zero;
             totalTime += Time.deltaTime;
             var integer = (int)totalTime; /* no need for now */
             yield return null;
@@ -87,19 +102,29 @@ public class LaserDrone : MonoBehaviour
 
     void Detect()
     {
-        Vector3 dir = (player.transform.position - transform.position).normalized;
+        Vector3 dir = (player.transform.position + new Vector3(0 , 5 , 0) - transform.position).normalized;
         float angle = Vector3.Angle(dir, transform.right);
         RaycastHit r;
         
-        if(Physics.Raycast(transform.position, dir, out r, range , layermask))
+        if(angle < fovAngle / 2)
         {
-            Debug.Log("df");
-            if(r.collider.gameObject != null)
+            
+            Debug.DrawLine(transform.position, dir, Color.green);
+            Debug.DrawRay(transform.position, dir);
+            if (Physics.Raycast(transform.position, dir, out r, range, layermask))
             {
-                Debug.Log(r.collider.gameObject.name);
+                Debug.Log("df");
+
+                if (r.collider.gameObject != null)
+                {
+                    Debug.Log(r.collider.gameObject.name);
+                    this.state = State.ATTACK;
+                }
             }
         }
-        this.state = State.ATTACK;
+        
+      
+        
     }
 
 }
