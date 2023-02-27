d08e5c9a6c6d3a476614e706b4b24fa64f1e90b3
Leo Ji <sj3026@nyu.edu>
2023-02-27 15:12:09 -0500

    fixing execution order of mapManager and CharacterSpawn

--- TrainGame/Assets/Scripts/MapManager.cs
+++ TrainGame/Assets/Scripts/MapManager.cs
@@ -19,7 +19,9 @@ public class MapManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //player = GameObject.FindGameObjectWithTag("Player");
+        player = GameObject.FindGameObjectWithTag("Player");
+        if (Singleton.Instance == null)
+            Debug.Log("singlton is null");
         id = Singleton.Instance.id;
         UpdatePlayerIcon();
         UpdatePlayer();
