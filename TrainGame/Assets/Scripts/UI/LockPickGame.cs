da812f49765401ae8f7e0c96c5ac0c17d72696c0
ROK <hilary0310@gmail.com>
2023-02-28 16:27:58 -0500

    Auto stash before merge of "main" and "origin/main"

--- TrainGame/Assets/Scripts/UI/LockPickGame.cs
+++ TrainGame/Assets/Scripts/UI/LockPickGame.cs
@@ -9,7 +9,7 @@ public class LockPickGame : MonoBehaviour
     [SerializeField] GameObject Inner;
     [SerializeField] GameObject Middle;
     [SerializeField] GameObject Outer;
-    [SerializeField] GameObject Hand;
+    [SerializeField] GameObject Hand;  //the handle
     [SerializeField] Vector2 InnerAngles; //vector should be organized by smaller then larger angle
     [SerializeField] Vector2 MiddleAngles;
     [SerializeField] Vector2 OuterAngles;
@@ -19,14 +19,18 @@ public class LockPickGame : MonoBehaviour
     public int iterator;
     public GameObject current;
     [SerializeField] AudioSource unlockSound;
-    // Start is called before the first frame update
+
+    private float areaAmt_Outer;
+    private float areaAmt_Middle;
+    private float areaAmt_Inner;
+
     void Start()
     {
         
 
         for(int i = 0; i < list.Count; i++)
         {
-            list[i].gameObject.GetComponent<Image>().color= new Color(0.2f,0.2f,0.2f,1f);
+            //list[i].gameObject.GetComponent<Image>().color= new Color(0.2f,0.2f,0.2f,1f);
         }
         iterator = 0;
     }
@@ -35,26 +39,26 @@ public class LockPickGame : MonoBehaviour
     void Update()
     {
         current = list[iterator];
-        list[iterator].gameObject.GetComponent<Image>().color = Color.white;
+        //list[iterator].gameObject.GetComponent<Image>().color = Color.white;
         Debug.Log("bruh:" + Middle.GetComponent<RectTransform>().eulerAngles.z);
         if (Input.GetKeyUp(KeyCode.Space))
         {
 
             if (current == Outer && Outer.GetComponent<RectTransform>().eulerAngles.z >= OuterAngles.x && Outer.GetComponent<RectTransform>().eulerAngles.z <= OuterAngles.y) //We have to check the certain angle range of values, collisions will mess UI up, and sorry its the simplest way for now :(
             {
-                list[iterator].gameObject.GetComponent<Image>().color = Color.black;
+                //list[iterator].gameObject.GetComponent<Image>().color = Color.black;
                 unlockSound.Play();
                 iterator++;
             } else if (current == Middle && Middle.GetComponent<RectTransform>().eulerAngles.z >= MiddleAngles.x && Middle.GetComponent<RectTransform>().eulerAngles.z <= MiddleAngles.y) //We have to check the certain angle range of values, collisions will mess UI up, and sorry its the simplest way for now :(
             {
-                list[iterator].gameObject.GetComponent<Image>().color = Color.black;
+                //list[iterator].gameObject.GetComponent<Image>().color = Color.black;
                 unlockSound.Play(); 
                 iterator++;
             }
             else if (current == Inner && Inner.GetComponent<RectTransform>().eulerAngles.z >= InnerAngles.x && Inner.GetComponent<RectTransform>().eulerAngles.z <= InnerAngles.y) //We have to check the certain angle range of values, collisions will mess UI up, and sorry its the simplest way for now :(
             {
                 unlockSound.Play();
-                list[iterator].gameObject.GetComponent<Image>().color = Color.black;
+                //list[iterator].gameObject.GetComponent<Image>().color = Color.black;
                 Complete = true;
             }
             else
@@ -62,5 +66,8 @@ public class LockPickGame : MonoBehaviour
                 //no more wheels
             }
         }
+    }
+    void CheckLock(){
+
     }
 }
