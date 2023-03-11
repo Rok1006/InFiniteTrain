e3b67e3d7c87089f27f281ec7c5f81bde5fa054a
Leo Ji <sj3026@nyu.edu>
2023-03-11 01:21:22 -0500

    teleporter setter

---
+++ TrainGame/Assets/Scripts/UI/AddFuelPanelUI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoreMountains.InventoryEngine;
+using UnityEngine.UI;
+
+
+/*this is custom ui class for add fuel panel*/
+public class AddFuelPanelUI : MonoBehaviour
+{
+    private List<InventorySlot> slots = new List<InventorySlot>();
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
