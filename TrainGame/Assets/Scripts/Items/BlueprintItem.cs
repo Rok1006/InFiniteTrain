56f47106c0aa4ed3072613e231dd0897d9d752f2
Keistie <mh5448@nyu.edu>
2023-04-03 16:20:02 -0400

    Blue Print items add in

---
+++ TrainGame/Assets/Scripts/Items/BlueprintItem.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoreMountains.InventoryEngine;
+using System;
+
+[CreateAssetMenu(fileName = "BlueprintItem", menuName = "InfiniteTrain/Inventory/BlueprintItem", order = 4)]
+[Serializable]
+public class BlueprintItem : InventoryItemPlus
+{
+    
+}
