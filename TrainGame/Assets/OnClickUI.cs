90bf5502d8764824fca593950433a076bada0c00
ReynahD <wl2357@nyu.edu>
2023-04-22 02:27:49 -0400

    desk stuff

---
+++ TrainGame/Assets/OnClickUI.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnClickUI : MonoBehaviour
+{
+    // Reference to the global image object
+    public Image globalImage;
+
+    // The sprite to display when this button is pressed
+    public Sprite spriteToShow;
+
+    // Called when the button is clicked
+    public void OnButtonClick()
+    {
+        // Set the global image's sprite to the sprite for this button
+        globalImage.sprite = spriteToShow;
+    }
+}
