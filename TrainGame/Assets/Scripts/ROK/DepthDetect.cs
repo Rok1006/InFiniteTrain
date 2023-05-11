2ee5a330fe8c5c79f4ebda5d721874c2ea3d1df3
ROK <hilary0310@gmail.com>
2023-05-11 03:55:16 -0400

    Depth shit

--- TrainGame/Assets/Scripts/ROK/DepthDetect.cs
+++ TrainGame/Assets/Scripts/ROK/DepthDetect.cs
@@ -12,9 +12,9 @@ public class DepthDetect : MonoBehaviour
     [SerializeField] private GameObject FrontMC;
     [SerializeField] private GameObject BackMC;
 
-    public float rayLength = 30f;
+    public float rayLength = 15f;
 
-    [ReadOnly] public int frontOrderIndex, backOrderIndex;
+    [ReadOnly] public int OrderIndex = 0;
     
 
     void Start()
@@ -25,43 +25,51 @@ public class DepthDetect : MonoBehaviour
 //Layer
     void Update(){ //RayCast Detect
         //int layerMask = LayerMask.GetMask("Enemies");
+        FrontMC.GetComponent<MeshRenderer>().sortingOrder = OrderIndex;
+        BackMC.GetComponent<MeshRenderer>().sortingOrder = OrderIndex;
         //back
-        Vector3 rayStartPos = new Vector3(transform.position.x, transform.position.y+0.99f,transform.position.z+0.5f);
-        if(Physics.Raycast(rayStartPos, Vector3.forward, rayLength)){
+        Vector3 rayStartPos = new Vector3(transform.position.x, transform.position.y,transform.position.z);
+        RaycastHit hitB;
+        if(Physics.Raycast(rayStartPos, Vector3.forward, out hitB, rayLength)){
             Debug.DrawRay(rayStartPos, new Vector3(0,0,rayLength), Color.green);
-            // Debug.Log("yep");
-            FrontMC.GetComponent<MeshRenderer>().sortingOrder = 1;
-            BackMC.GetComponent<MeshRenderer>().sortingOrder = 1;
+            Debug.Log(hitB.transform.gameObject.name);
+            CheckLayer(hitB.transform.gameObject);
         }
         Debug.DrawRay(rayStartPos, new Vector3(0,0,rayLength), Color.red);
 
         //Front
         //Vector3 rayStartPos1 = new Vector3(transform.position.x, transform.position.y+0.99f,transform.position.z+0.5f);
-        if(Physics.Raycast(rayStartPos, -Vector3.forward, rayLength)){
+        RaycastHit hitF;
+        if(Physics.Raycast(rayStartPos, -Vector3.forward, out hitF, rayLength)){
             Debug.DrawRay(rayStartPos, new Vector3(0,0,-rayLength), Color.green);
-            //Debug.Log("yep");
-            FrontMC.GetComponent<MeshRenderer>().sortingOrder = -1;
-            BackMC.GetComponent<MeshRenderer>().sortingOrder = -1;
+            Debug.Log(hitF.transform.gameObject.name);
+            CheckLayer(hitF.transform.gameObject);
         }
         Debug.DrawRay(rayStartPos, new Vector3(0,0,-rayLength), Color.red);
 
-        RaycastHit hitL;
-        if(Physics.Raycast(rayStartPos, Vector3.left, out hitL, rayLength)){
-            CheckLayer(hitL.transform.gameObject);
-        }
-        RaycastHit hitR;
-        if(Physics.Raycast(rayStartPos, -Vector3.left, out hitR, rayLength)){
-            CheckLayer(hitR.transform.gameObject);
-        }
+        // RaycastHit hitL;
+        // if(Physics.Raycast(rayStartPos, Vector3.left, out hitL, rayLength)){
+        //     CheckLayer(hitL.transform.gameObject);
+        //     //Debug.Log(hitL.transform.gameObject.name);
+        // }
+        // RaycastHit hitR;
+        // if(Physics.Raycast(rayStartPos, -Vector3.left, out hitR, rayLength)){
+        //     CheckLayer(hitR.transform.gameObject);
+        //     //Debug.Log(hitL.transform.gameObject.name);
+        // }
     
     }
     public void CheckLayer(GameObject obj){
-        if(obj.transform.position.z>Main.transform.position.z){ //behind
-            FrontMC.GetComponent<MeshRenderer>().sortingOrder = 1;
-            BackMC.GetComponent<MeshRenderer>().sortingOrder = 1;
-        }else if(obj.transform.position.z<Main.transform.position.z){ //infront
-            FrontMC.GetComponent<MeshRenderer>().sortingOrder = -1;
-            BackMC.GetComponent<MeshRenderer>().sortingOrder = -1;
+        if(obj.tag == "Environment"){
+            var so_obj = obj.transform.GetChild(0);
+            int obj_currentIndex = so_obj.GetComponent<DepthDetect_Other>().thisObjIndex;
+            Debug.Log(obj_currentIndex);
+            if(obj.transform.position.z>Main.transform.position.z){ //behind
+                OrderIndex = obj_currentIndex+1;
+            }else if(obj.transform.position.z<Main.transform.position.z){ //infront
+                OrderIndex = obj_currentIndex-1;
+            }
         }
+        
     }
 }
--- TrainGame/Assets/Scripts/ROK/DepthDetect_Other.cs
+++ TrainGame/Assets/Scripts/ROK/DepthDetect_Other.cs
@@ -1,27 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Spine;
+using Spine.Unity;
+using NaughtyAttributes;
 //This is a depth detect obj thatattach on each of the character or enviroenment obj
 //all thign sshd be on the same layer: Character & Depth
 //thing will be in update to constantly detect 
 
 public class DepthDetect_Other : MonoBehaviour
 {
-    public enum ObjType { ENEMY, ENVIRO }
-    public float rayLength = 30f;
+    public enum ObjType { NONE, ENEMY, ENVIRO }
+    public ObjType CurrentObjType = ObjType.NONE;
+    [SerializeField] private float rayLength = 15f;
+    [SerializeField] private GameObject ObjWithSortingLayer;
+    public int thisObjIndex;
+    [ReadOnly] public int OrderIndex = 0;
+
     
     void Start()
     {
-        
+        RayDetect();
     }
 
     void Update()
     {
-        
+        thisObjIndex = ObjWithSortingLayer.GetComponent<MeshRenderer>().sortingOrder;
+        ObjWithSortingLayer.GetComponent<MeshRenderer>().sortingOrder = OrderIndex;
+    }
+    void RayDetect(){
+        //back
+        Vector3 rayStartPos = new Vector3(transform.position.x, transform.position.y,transform.position.z);
+        RaycastHit hitB;
+        if(Physics.BoxCast(rayStartPos,this.transform.localScale / 2.0f, Vector3.forward, out hitB, Quaternion.identity, rayLength)){
+            Debug.DrawRay(rayStartPos, new Vector3(0,0,rayLength), Color.green);
+            //Debug.Log(hitB.transform.gameObject.name);
+            CheckLayer(hitB.transform.gameObject);
+        }
+        Debug.DrawRay(rayStartPos, new Vector3(0,0,rayLength), Color.red);
+
+        //Front
+        //Vector3 rayStartPos1 = new Vector3(transform.position.x, transform.position.y+0.99f,transform.position.z+0.5f);
+        RaycastHit hitF;
+        if(Physics.BoxCast(rayStartPos, this.transform.localScale / 2.0f,-Vector3.forward, out hitF, Quaternion.identity,  rayLength)){
+            Debug.DrawRay(rayStartPos, new Vector3(0,0,-rayLength), Color.green);
+            //Debug.Log(hitF.transform.gameObject.name);
+            CheckLayer(hitF.transform.gameObject);
+        }
+        Debug.DrawRay(rayStartPos, new Vector3(0,0,-rayLength), Color.red);
     }
 
     public void CheckLayer(GameObject obj){
-        
+        if(obj.tag == "Environment"){
+            var so_obj = obj.transform.GetChild(0);
+            int obj_currentIndex = so_obj.GetComponent<DepthDetect_Other>().thisObjIndex;
+            Debug.Log(obj_currentIndex);
+            if(obj.transform.position.z>this.transform.position.z){ //behind
+                OrderIndex = obj_currentIndex+1;
+            }else if(obj.transform.position.z<this.transform.position.z){ //infront
+                OrderIndex = obj_currentIndex-1;
+            }
+        }
 
     }
 }
